eea2f52a4e16dd18b6e0ee6b118dfe5fe105de23
viniciuscbm <viniciusacbm@gmail.com>
2018-12-09 12:47:31 -0300

    create model Quote

---
+++ app/models/quote.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Quote
+  include Mongoid::Document
+
+  field :quote,        type: String
+  field :author,       type: String
+  field :author_about, type: String
+  field :tags,         type: Array
+
+  validates :quote, :author, :author_about, :tags, presence: true
+end
