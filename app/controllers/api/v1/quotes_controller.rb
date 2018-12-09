25107f6c48d7faee1a5522021243ff42449f8556
viniciuscbm <viniciusacbm@gmail.com>
2018-12-09 14:46:50 -0300

    add search in Quotes Controller

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 module Api::V1
-  class QuotesController < ApiController
+	class QuotesController < ApiController
+		before_action :set_quotes
+
 		def index
-			render json: {}
-    end
+			render json: @quotes
+		end
+		
+		private
+
+		def set_quotes
+			@quotes = Quote.where(:tags.in => [params[:tags]])
+		end
+
+		def quote_params
+			params.permit(:tags)
+		end
+
   end
 end
