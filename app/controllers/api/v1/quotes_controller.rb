014e3c8440fc049220035f5d9c479d79a5585c1b
viniciuscbm <viniciusacbm@gmail.com>
2018-12-09 13:26:25 -0300

    create controller Quotes, routes and setup autentication

---
+++ app/controllers/api/v1/quotes_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Api::V1
+  class QuotesController < ApiController
+		def index
+			render json: {}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
+      resources :quotes, only: %i[index]
     end
   end
 end
